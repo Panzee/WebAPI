95585b887b1c6a5f4fd664df73658db33f1fae69
Panzee <hyk1c1b3221@gmail.com>
2017-09-03 21:49:19 +0900

    Revert "dmm api xmlからitem取得まで"
    This reverts commit 1d6309ea6f62c9dd1e57657302042609a09dd8d4.

--- class/dmm.php
+++ class/dmm.php
@@ -5,9 +5,6 @@ class DmmAPI {
 	private $api_id;
 	private $affiliate_id;
 
-	private $url;
-	private $xml;
-
 	public function __construct( $api_id, $affiliate_id ) {
 		$this->api_id = $api_id;
 		$this->affiliate_id = $affiliate_id;
@@ -31,39 +28,7 @@ class DmmAPI {
 		foreach( $params as $k => $v ) {
 			$url .= $k . '=' . $v . '&';
 		}
-		$this->url = substr( $url, 0, -1 );
-		return $this->url;
-	}
-
-	/**
-	*	get response XML type
-	*
-	* @param $url string
-	* @return string
-	*/
-	public function getResponseXML( $url ) {
-		$this->xml = simplexml_load_string( file_get_contents( $url ) );
-		return $this->xml;
-	}
-
-	/**
-	*	get response XML type
-	*
-	* @param $xml object
-	* @return array
-	*/
-	public function getResponseAll( $xml ) {
-		$items = [];
-		foreach( $xml->result->items->item as $item ) {
-			$items[] = [ 'title' => $item->title,
-									 'URL' => $item->URL,
-									 'affiliateURL' => $item->affiliateURL,
-									 'imageURL'=> $item->imageURL->large,
-									 'sampleMovieURL'=> $item->sampleMovieURL->size_720_480,
-									 'date'=> $item->date,
-								 ];
-		}
-		return $items;
+		return substr( $url, 0, -1 );
 	}
 
 }
